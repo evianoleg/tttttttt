ff03bf1f3c56f8ffd874834aec0334245480aae9
evianoleg <flober202@gmail.com>
2016-06-22 19:48:37 +0300

    firt c

--- Wallet/Program.cs
+++ Wallet/Program.cs
@@ -3,13 +3,17 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using money;
 namespace Wallet
 {
     class Program
     {
         static void Main(string[] args)
         {
+
+            Money m = new Money();
+            
+            
             int myMoney = 100;
 
         }
